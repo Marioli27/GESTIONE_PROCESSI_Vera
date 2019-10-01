b6db9cd9242230a919897d7c41b023f7bafce7de
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-01 09:37:17 +0200

    Aprire più file tramite "Process.Start"

--- Gestione_Processi_Vera/APPLICAZIONEPROCESSI/Program.cs
+++ Gestione_Processi_Vera/APPLICAZIONEPROCESSI/Program.cs
@@ -13,7 +13,12 @@ namespace APPLICAZIONEPROCESSI
         {
             Console.WriteLine("Esercitazione sulla gestione dei progetti");
             Process.Start("Notepad.exe");
-            Process.Start(@"C:\Users\studenti\Source\Repos\GESTIONE_PROCESSI_Vera\Gestione_Processi_Vera\APPLICAZIONEPROCESSI\FileDiTesto.txt");
+            Process.Start("Notepad.exe",@"C:\Users\studenti\Source\Repos\GESTIONE_PROCESSI_Vera\Gestione_Processi_Vera\APPLICAZIONEPROCESSI\FileDiTesto.txt");
+            Process.Start("Chrome.exe",@"www.snake.it");
+            var app = new Process();
+            app.StartInfo.FileName = @"Notepad.exe";
+            app.StartInfo.Arguments = @"C:\Users\studenti\Source\Repos\GESTIONE_PROCESSI_Vera\Gestione_Processi_Vera\APPLICAZIONEPROCESSI\FileDiTesto.txt";
+            app.Start();
         }
     }
 }
